ecdf61261c6d7cb5766d7cf91981ab044a555d23
xjd <lynndon@gmail.com>
2024-10-14 10:27:05 +0800

    feat: Implement I/O traits for common types (#3)
    * feat: Implement I/O traits for common types
    
    * fix: Refactor IPC code to use fully qualified paths and improve structure

--- crates/ckb-script-ipc-common/src/io_impl.rs
+++ crates/ckb-script-ipc-common/src/io_impl.rs
@@ -1,76 +1,126 @@
-// from: https://github.com/rust-embedded/embedded-hal/tree/master/embedded-io/src/impls
+// from standard library
 extern crate alloc;
 use crate::error::IpcError;
 use crate::io::{BufRead, Read, Seek, SeekFrom, Write};
 use alloc::boxed::Box;
+use alloc::collections::vec_deque::VecDeque;
 use alloc::vec::Vec;
+use core::cmp;
+use core::mem;
 
-impl<T: ?Sized + Read> Read for Box<T> {
-    type Error = T::Error;
+// =============================================================================
+// Forwarding implementations
+
+impl<R: Read + ?Sized> Read for &mut R {
+    type Error = R::Error;
     #[inline]
     fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
-        T::read(self, buf)
+        (**self).read(buf)
     }
 }
 
-impl<T: ?Sized + BufRead> BufRead for Box<T> {
-    type Error = T::Error;
-    fn fill_buf(&mut self) -> Result<&[u8], Self::Error> {
-        T::fill_buf(self)
-    }
-
-    fn consume(&mut self, amt: usize) {
-        T::consume(self, amt);
-    }
-}
-
-impl<T: ?Sized + Write> Write for Box<T> {
-    type Error = T::Error;
+impl<W: Write + ?Sized> Write for &mut W {
+    type Error = W::Error;
     #[inline]
     fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
-        T::write(self, buf)
+        (**self).write(buf)
     }
 
     #[inline]
     fn flush(&mut self) -> Result<(), Self::Error> {
-        T::flush(self)
+        (**self).flush()
     }
 }
 
-impl<T: ?Sized + Seek> Seek for Box<T> {
-    type Error = T::Error;
+impl<S: Seek + ?Sized> Seek for &mut S {
+    type Error = S::Error;
     #[inline]
     fn seek(&mut self, pos: SeekFrom) -> Result<u64, Self::Error> {
-        T::seek(self, pos)
+        (**self).seek(pos)
     }
 }
 
-impl Write for &mut [u8] {
-    type Error = IpcError;
+impl<B: BufRead + ?Sized> BufRead for &mut B {
+    type Error = B::Error;
+    #[inline]
+    fn fill_buf(&mut self) -> Result<&[u8], Self::Error> {
+        (**self).fill_buf()
+    }
+
+    #[inline]
+    fn consume(&mut self, amt: usize) {
+        (**self).consume(amt)
+    }
+}
+
+impl<R: Read + ?Sized> Read for Box<R> {
+    type Error = R::Error;
+    #[inline]
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
+        (**self).read(buf)
+    }
+}
+
+impl<W: Write + ?Sized> Write for Box<W> {
+    type Error = W::Error;
     #[inline]
     fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
-        let amt = core::cmp::min(buf.len(), self.len());
-        if !buf.is_empty() && amt == 0 {
-            return Err(IpcError::SliceWriteError);
-        }
-        let (a, b) = core::mem::take(self).split_at_mut(amt);
-        a.copy_from_slice(&buf[..amt]);
-        *self = b;
-        Ok(amt)
+        (**self).write(buf)
+    }
+    #[inline]
+    fn flush(&mut self) -> Result<(), Self::Error> {
+        (**self).flush()
     }
 
     #[inline]
-    fn flush(&mut self) -> Result<(), Self::Error> {
-        Ok(())
+    fn write_all(&mut self, buf: &[u8]) -> Result<(), Self::Error> {
+        (**self).write_all(buf)
     }
 }
 
+impl<S: Seek + ?Sized> Seek for Box<S> {
+    type Error = S::Error;
+    #[inline]
+    fn seek(&mut self, pos: SeekFrom) -> Result<u64, Self::Error> {
+        (**self).seek(pos)
+    }
+
+    #[inline]
+    fn stream_position(&mut self) -> Result<u64, Self::Error> {
+        (**self).stream_position()
+    }
+}
+
+impl<B: BufRead + ?Sized> BufRead for Box<B> {
+    type Error = B::Error;
+    #[inline]
+    fn fill_buf(&mut self) -> Result<&[u8], Self::Error> {
+        (**self).fill_buf()
+    }
+
+    #[inline]
+    fn consume(&mut self, amt: usize) {
+        (**self).consume(amt)
+    }
+}
+
+// =============================================================================
+// In-memory buffer implementations
+
+/// Read is implemented for `&[u8]` by copying from the slice.
+///
+/// Note that reading updates the slice to point to the yet unread part.
+/// The slice will be empty when EOF is reached.
 impl Read for &[u8] {
     type Error = IpcError;
     #[inline]
     fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
-        let amt = core::cmp::min(buf.len(), self.len());
+        let amt = cmp::min(buf.len(), self.len());
         let (a, b) = self.split_at(amt);
+
+        // First check if the amount of bytes we want to read is small:
+        // `copy_from_slice` will generally expand to a call to `memcpy`, and
+        // for a single byte the overhead is significant.
         if amt == 1 {
             buf[0] = a[0];
         } else {
@@ -95,12 +145,33 @@ impl BufRead for &[u8] {
     }
 }
 
-impl Write for Vec<u8> {
+/// Write is implemented for `&mut [u8]` by copying into the slice, overwriting
+/// its data.
+///
+/// Note that writing updates the slice to point to the yet unwritten part.
+/// The slice will be empty when it has been completely overwritten.
+///
+/// If the number of bytes to be written exceeds the size of the slice, write operations will
+/// return short writes: ultimately, `Ok(0)`; in this situation, `write_all` returns an error of
+/// kind `ErrorKind::WriteZero`.
+impl Write for &mut [u8] {
     type Error = IpcError;
     #[inline]
-    fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
-        self.extend_from_slice(buf);
-        Ok(buf.len())
+    fn write(&mut self, data: &[u8]) -> Result<usize, Self::Error> {
+        let amt = cmp::min(data.len(), self.len());
+        let (a, b) = mem::take(self).split_at_mut(amt);
+        a.copy_from_slice(&data[..amt]);
+        *self = b;
+        Ok(amt)
+    }
+
+    #[inline]
+    fn write_all(&mut self, data: &[u8]) -> Result<(), Self::Error> {
+        if self.write(data)? == data.len() {
+            Ok(())
+        } else {
+            Err(IpcError::SliceWriteError)
+        }
     }
 
     #[inline]
@@ -109,15 +180,77 @@ impl Write for Vec<u8> {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::io::Read;
-    #[test]
-    fn test_io_impl() {
-        let array = [1u8, 2, 3, 4];
-        let mut buf = &array[..];
-        let buf2 = &mut [0u8; 2];
-        buf.read(buf2).unwrap();
-        assert_eq!(buf.len(), 2);
+/// Write is implemented for `Vec<u8>` by appending to the vector.
+/// The vector will grow as needed.
+impl Write for Vec<u8> {
+    type Error = IpcError;
+    #[inline]
+    fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
+        self.extend_from_slice(buf);
+        Ok(buf.len())
+    }
+    #[inline]
+    fn write_all(&mut self, buf: &[u8]) -> Result<(), Self::Error> {
+        self.extend_from_slice(buf);
+        Ok(())
+    }
+
+    #[inline]
+    fn flush(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+/// Read is implemented for `VecDeque<u8>` by consuming bytes from the front of the `VecDeque`.
+impl Read for VecDeque<u8> {
+    type Error = IpcError;
+    /// Fill `buf` with the contents of the "front" slice as returned by
+    /// [`as_slices`][`VecDeque::as_slices`]. If the contained byte slices of the `VecDeque` are
+    /// discontiguous, multiple calls to `read` will be needed to read the entire content.
+    #[inline]
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
+        let (ref mut front, _) = self.as_slices();
+        let n = Read::read(front, buf)?;
+        self.drain(..n);
+        Ok(n)
+    }
+}
+
+/// BufRead is implemented for `VecDeque<u8>` by reading bytes from the front of the `VecDeque`.
+impl BufRead for VecDeque<u8> {
+    type Error = IpcError;
+    /// Returns the contents of the "front" slice as returned by
+    /// [`as_slices`][`VecDeque::as_slices`]. If the contained byte slices of the `VecDeque` are
+    /// discontiguous, multiple calls to `fill_buf` will be needed to read the entire content.
+    #[inline]
+    fn fill_buf(&mut self) -> Result<&[u8], Self::Error> {
+        let (front, _) = self.as_slices();
+        Ok(front)
+    }
+
+    #[inline]
+    fn consume(&mut self, amt: usize) {
+        self.drain(..amt);
+    }
+}
+
+/// Write is implemented for `VecDeque<u8>` by appending to the `VecDeque`, growing it as needed.
+impl Write for VecDeque<u8> {
+    type Error = IpcError;
+    #[inline]
+    fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
+        self.extend(buf);
+        Ok(buf.len())
+    }
+
+    #[inline]
+    fn write_all(&mut self, buf: &[u8]) -> Result<(), Self::Error> {
+        self.extend(buf);
+        Ok(())
+    }
+
+    #[inline]
+    fn flush(&mut self) -> Result<(), Self::Error> {
+        Ok(())
     }
 }
